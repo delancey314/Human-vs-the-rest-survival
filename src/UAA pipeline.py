c2ae07e526b274fcf1470a8e612792265106179e
Bobcat90 <delancey314@netzero.net>
2019-10-14 11:54:02 -0600

    regular backup.  Just lost drought

---
+++ src/UAA pipeline.py
@@ -0,0 +1,34 @@
+import pandas as pd 
+rom collections import defaultdict
+from collections import Counter
+
+class UAAPipeline(self):
+    def __init__(self,filename='results'):
+        self.filename=filename
+        self.temp_file=temp_file
+        self.old_features=old_features
+        self.clean_dict=defaultdict{lambda:'None',
+                                    'drought_data':{'filename':'data/Drought Data.csv',
+                                                    'clean':      }
+     
+                                    }
+
+
+
+    def load(self, parameter_list):
+        self.temp_file=pd.read_csv(f'data/{self.filename}.csv')
+
+    def rename(self):
+        self.old_features=self.temp_file.columns
+        if self.filename in self.clean_list:
+            
+    def drop_columns(self, parameter_list):
+        pass
+
+    def if __name__ == "__main__":
+        load(filename)
+        rename()
+        clean()
+        create_columns()
+        return output_file
+        pass
