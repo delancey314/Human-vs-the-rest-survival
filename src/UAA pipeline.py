92843defc1f99b7f902803c8296bb820d04b9e04
Mike De Lancey <52059049+delancey314@users.noreply.github.com>
2019-10-17 20:29:17 -0600

    working on trees code

--- src/UAA pipeline.py
+++ src/UAA pipeline.py
@@ -9,7 +9,17 @@ class UAAPipeline(self,filename):
         self.old_features=old_features
         self.new_name=new_name
         self.features_dict=defaultdict({lambda:'None'})  
-        self.new_names_dict=dict('city':['city', 'state', 'geo_id','overall_risk', 'overall_readiness'],\
+        self.new_names_dict={'risk':['city', 'state', 'geo_id','overall_risk', 'overall_readiness'],\
+                            'city':['city','state','geo_id','GDP_water_all','perc_fff','d_plan','w_plan',\
+                                    'base_w_stress','invest','tax_break','corruption','perc_vote','innovate',\
+                                    'climate_real','hs_ed','pop_dens','w_quality','debt','flood_pop','flood_build',\
+                                    'flood_car','impervious','beds_1000','alone_65','child_5','health_ins','older_1999',\
+                                    'mobile_home','rent_50plus','trees','heating','disabled','poverty','older_1979',\
+                                    'work_outside','no_car','pop_under_1ft','pop_under_3ft','impact_plus1ft','impact_plus3ft',
+                                    'col_empty','lat','long','county','region','city_km2','pop_2010','pop_2011','pop_2012','pop_2013',\
+                                    'pop_2014','pop_2015','median_income']}
+
+
 
     #lambda function to add categories to a file
      def categorize_(row):
@@ -24,32 +34,36 @@ class UAAPipeline(self,filename):
 
 
     def file_exist(self):
-        if self.filename == 'city':
-            self.work_file=pd.read_csv('data/transformed_data/City Indicators.csv')
+        if  self.filename == 'city':
+            self.work_file=pd.read_csv('../data/transformed_data/City Indicators.csv')
         elif self.filename == 'drought':
-            self.work_file=pd.read_csv('data/transformed_data/Drought Data.csv')
+            self.work_file=pd.read_csv('../data/transformed_data/Drought Data.csv')
         elif self.filename == 'flood':   
-            self.work_file=pd.read_csv('data/transformed_data/Flood Data.csv')
+            self.work_file=pd.read_csv('../data/transformed_data/Flood Data.csv')
         elif self.filename == 'heat':   
-            self.work_file=pd.read_csv('data/transformed_data/Heat Data.csv')
+            self.work_file=pd.read_csv('../data/transformed_data/Heat Data.csv')
         elif self.filename == 'risk':
-            self.work_file=pd.read_csv('data/transformed_data/Overall Risk & Readiness Scores.csv')
+            self.work_file=pd.read_csv('../data/transformed_data/Overall Risk & Readiness Scores.csv')
         elif self.filename == 'coastal':
-            self.work_file=pd.read_csv('data/transformed_data/Sea Level Rise Data.csv')
-        
+            self.work_file=pd.read_csv('../data/transformed_data/Sea Level Rise Data.csv')
+        elif self.filename == 'cold':
+             self.work_file=pd.read_csv('../data/transformed_data/Cold Data.csv')
+
     def new_load(self):
-        if self.filename == 'city':
-            self.work_file=pd.read_csv('data/raw_data/City Indicators.csv')
+        if  self.filename == 'city':
+            self.work_file=pd.read_csv('../data/raw_data/City Indicators.csv')
         elif self.filename == 'drought':
-            self.work_file=pd.read_csv('data/raw_data/Drought Data.csv')
+            self.work_file=pd.read_csv('../data/raw_data/Drought Data.csv')
         elif self.filename == 'flood':   
-            self.work_file=pd.read_csv('data/raw_data/Flood Data.csv')
+            self.work_file=pd.read_csv('../data/raw_data/Flood Data.csv')
         elif self.filename == 'heat':   
-            self.work_file=pd.read_csv('data/raw_data/Heat Data.csv')
+            self.work_file=pd.read_csv('../data/raw_data/Heat Data.csv')
         elif self.filename == 'risk':
-            self.work_file=pd.read_csv('data/raw_data/Overall Risk & Readiness Scores.csv')
+            self.work_file=pd.read_csv('../data/raw_data/Overall Risk & Readiness Scores.csv')
         elif self.filename == 'coastal':
-            self.work_file=pd.read_csv('data/raw_data/Sea Level Rise Data.csv')
+            self.work_file=pd.read_csv('../data/raw_data/Sea Level Rise Data.csv')
+        elif self.filename == 'cold':
+             self.work_file=pd.read_csv('../data/raw_data/Cold Data.csv')
         else:
             print('file not in list. Please choose from city,drought,flood,heat,risk or coastal')
             break
@@ -59,30 +73,69 @@ class UAAPipeline(self,filename):
         self.old_features=self.work_file.columns
         self.new_name=self.new_names_dict[self.filename]
         self.filename.columns=self.new_names
+        temp_dict_= dict(zip(self.new_name,self.old_features))
+        self.features_dict[self.filename]=temp_dict_
+        temp_dict_={}
 
             
     
-    def drop_fields(self):
+    def clean(self):
         #Puerto Rico is being dropped at rows [19, 35, 193, 220] because of data inconsistency
-        self.workfile.drop([19, 35, 193, 220], inplace = True)
+        self.work_file.drop([19, 35, 193, 220], inplace = True)
         if self.filename == 'city':
-           
+            self.work_file.drop(columns =['col_empty'], inplace= True)
+            #two cities had their order of magnitude incorrect for % of people in floodzone. Their neighboring values match the lower magnitude'
+            self.work_file=self.work_file.replace(412.9208681,41.29208681)
+            self.work_file=self.work_file.replace(101.6959823,10.16959823)
+            self.work_file['d_plan'] = self.work_file['d_plan'].map({'Yes': 1, 'No': 0})
+            self.work_file['w_plan'] = self.work_file['w_plan'].map({'Yes': 1, 'No': 0})
+            self.work_file.drop(columns=['city','state','geo_id','county', 'region','pop_2010', 'pop_2011', 'pop_2012', 'pop_2013', 'pop_2014'], inplace=True)
+            self.work_file.fillna(0, inplace=True)
+            try:
+                indices=pd.read_csv('../data/raw_data/Overall Risk & Readiness Scores.csv')
+            except:
+                load('risk')
+            self.work_file['risk_ready_cat']=indices['risk_ready_cat']
+
         elif self.filename == 'drought':
-           
+           pass
         elif self.filename == 'flood':   
-            
+            pass
         elif self.filename == 'heat':   
-            
+            pass
+        elif self.filename == 'cold':
+            pass
         elif self.filename == 'risk':
-           self.workfile['overall_readiness'] = np.where(self.workfile['overall_readiness']>=.4651, 1, 0)
-           self.workfile['overall_risk'] = np.where(self.workfile['overall_risk']>=.3840, 1, 0)
-           self.workfile['overall_readiness'] = np.where(self.workfile['overall_readiness']>=.4651, 1, 0)
-           self.workfile['risk_ready_cat']=self.workfile.apply(lambda row: categorize(row), axis=1)
+            self.work_file['overall_readiness'] = np.where(self.work_file['overall_readiness']>=.4651, 1, 0)
+            self.work_file['overall_risk'] = np.where(self.work_file['overall_risk']>=.3840, 1, 0)
+            self.work_file['overall_readiness'] = np.where(self.work_file['overall_readiness']>=.4651, 1, 0)
+            self.work_file['risk_ready_cat']=self.work_file.apply(lambda row: categorize_(row), axis=1)
+            cities=self.work_file['city']
+            cities.to_csv('../data/transformed_data/cities.csv')
+
+            elif self.filename == 'coastal':
+
+    def write_and_return():
+        if self.filename == 'city':
+            self.work_file.to_csv('../data/raw_data/City Indicators.csv')
+            return self.work_file
+        elif self.filename == 'drought':
+            self.work_file.to_csv('../data/raw_data/Drought Data.csv')
+        elif self.filename == 'flood':   
+            self.work_file.to_csv('../data/raw_data/Flood Data.csv')
+        elif self.filename == 'heat':   
+            self.work_file.to_csv('../data/raw_data/Heat Data.csv')
+        elif self.filename == 'risk':
+            self.work_file.to_csv('../data/raw_data/Overall Risk & Readiness Scores.csv')
         elif self.filename == 'coastal':
+            self.work_file.to_csv('../data/raw_data/Sea Level Rise Data.csv')
+        else:
+            self.work_file.to_csv('../data/raw_data/Cold Data.csv')
+      
+            
 
-    def 
 
-    def make_file(self,filename):
+    def  make_file(self,filename):
         self.filename=filename
         try:
             file_exist()
@@ -90,7 +143,8 @@ class UAAPipeline(self,filename):
         except:
             new_load()
             rename()
-            clean_columns()
+            clean()
+            write_and_return()
 
         
     
--- src/main.py
+++ src/main.py
@@ -1,21 +1,49 @@
 import numpy as np 
 import pandas as pd 
 import UAAPipeline
+from collections import defaultdict
+import matplotlib.pyplot as plt
 
+from xgboost import XGBClassifier
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn import metrics
 
+import numpy as np
+from scipy import interp
+import matplotlib.pyplot as plt
+
+from sklearn import svm, datasets
+from sklearn.metrics import roc_curve, auc
+
+pd.options.display.max_columns = None
 
 city_indicators=pd.read_csv('data/raw_data/City Indicators.csv')
 drought_data=pd.read_csv('data/raw_data/Drought Data.csv')
 flood_data=pd.read_csv('data/raw_data/Flood Data.csv')
 heat_data=pd.read_csv('data/raw_data/Heat Data.csv')
+######
 historical=pd.read_csv('data/raw_data/Historical Hazards Outcome Data.csv')
+
 risk_recovery=pd.read_csv('data/raw_data/Overall Risk & Readiness Scores.csv')
 sea_level_data=pd.read_csv('data/raw_data/Sea Level Rise Data.csv')
 
 pipe=UAAPipeline()
 
+def
+
+
 
 if __name__ == "__main__":
-    risk_ready=pipe.make_file('risk')
+    risk_readiness=pipe.make_file('risk')
+    city_indicators=pipe.make_file('city')
     
+    
+    
+    trees_cat=city_ind['risk_ready_cat']
+trees_data = city_ind.drop(columns=['risk_ready_cat'])
+tree_features=trees_data.columns
+trees_cat.to_csv('data/transformed_data/classification_labels.csv')
+trees_data.to_csv('data/transformed_data/classification_data.csv')
+tree_feature.to_csv('data/transformed_data/classification_features.csv')
     pass
